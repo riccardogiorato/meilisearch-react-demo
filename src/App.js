d2091fd5f408b9040edf77869e495965383d6a48
Riccardo Giorato <giorat.96@gmail.com>
2020-04-10 15:09:57 +0200

    remove useless parts

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import React, { useState, useEffect } from "react";
 import MeiliSearch from "meilisearch";
 
 const client = new MeiliSearch({
-  host: "http://178.62.201.86:7700/",
-  apiKey: "59c29bd271e4c0eac4edc054f35e02224cbe9665b59f2adb532c901e516a0d15",
+  host: "http://127.0.0.1:7700/",
 });
 const index = client.getIndex("decathlon");
 
@@ -93,38 +92,6 @@ function App() {
       </div>
 
       <div>
-        {/*<div class="filters mx-2 mt-2">
-          <h3 class="text-xl text-black font-semibold justify-center px-2 py-4 inline-flex">
-            Filters
-          </h3>
-          <div class="relative inline-flex">
-            <svg
-              class="w-3 h-3 absolute top-0 right-0 m-5 pointer-events-none mr-2"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 412 232"
-            >
-              <path
-                d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z"
-                fill="#648299"
-                fill-rule="nonzero"
-              />
-            </svg>
-            <select class="p-6 pl-2 my-1 py-1 border border-gray-300 text-gray-600 h-10 bg-white hover:border-gray-400 focus:outline-none appearance-none">
-              <option>Choose a color</option>
-              <option>Red</option>
-              <option>Blue</option>
-              <option>Yellow</option>
-              <option>Black</option>
-              <option>Orange</option>
-              <option>Purple</option>
-              <option>Gray</option>
-              <option>White</option>
-            </select>
-          </div>
-          <button class="mx-2 bg-transparent hover:bg-black text-black-700 font-semibold hover:text-white py-2 px-3 border border-black-500 hover:border-transparent rounded">
-            Reset Filters
-          </button>
-  </div>*/}
 
         <div class="flex flex-wrap searchResults">{resultCards}</div>
       </div>
